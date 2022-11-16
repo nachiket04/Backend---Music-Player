4f13bc2efb86c75467543f2d4e97979a599e63d5
Nachiket Khadage <khadagenachi4@gmail.com>
2022-11-16 13:35:40 +0530

    added play next and prev

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    static LinkedList <Song> playList;
+    static ListIterator <Song> itr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Song s1 = new Song("Kesariya", 4.20);
@@ -12,7 +14,8 @@ public class Main {
         ArjitSingh.findSong("Kesariya");
         Album NehaKakkar = new Album("Best of Neha Kakkar", "Neha Kakkar");
         NehaKakkar.addSong("Kata laga", 3.15);
-        LinkedList <Song> playList = new LinkedList<>();
+        playList = new LinkedList<>();
+        itr = playList.listIterator();
         ArjitSingh.addToPlaylist("Kesariya", playList);
         NehaKakkar.addToPlaylist("kata Laga", playList);
 //        printPlayList(playList);
@@ -25,8 +28,20 @@ public class Main {
                     return;
                 case 1:
                     showMenu();
+                    break;
                 case 2:
                     printPlayList(playList);
+                    itr = playList.listIterator();
+                    break;
+                case 3:
+                    play(playList);
+                    break;
+                case 4:
+                    playNext(playList);
+                    break;
+                case 5:
+                    playPrev(playList);
+                    break;
             }
         }
 
@@ -35,7 +50,7 @@ public class Main {
     public static void printPlayList(LinkedList <Song> playList){
         ListIterator <Song> itr = playList.listIterator();
         while(itr.hasNext()){
-            System.out.println("Now Playing " + itr.next());
+            System.out.println(itr.next());
         }
     }
 
@@ -43,11 +58,41 @@ public class Main {
         System.out.println("0. Exit");
         System.out.println("1. Print Menu");
         System.out.println("2. Show the list of all Songs in the playlist");
-        System.out.println("3. Play next song");
-        System.out.println("4. Play previous Song");
-        System.out.println("5. Repeat the song");
-        System.out.println("6. Delete the song");
+        System.out.println("3. Start Playing song");
+        System.out.println("4. Play next song");
+        System.out.println("5. Play previous Song");
+        System.out.println("6. Repeat the song");
+        System.out.println("7. Delete the song");
 
+    }
+
+    public static boolean play(LinkedList <Song> playList){
+        if(!itr.hasNext()){
+            System.out.println("PlayList is Empty..!");
+            return false;
+        }
+        System.out.println("Now playing " + itr.next());
+        return true;
+    }
+
+    public static boolean playNext(LinkedList <Song> playList){
+
+        if(!itr.hasNext()){
+            System.out.println("End of the PlayList.." + "\n" + "Nothing to play.. :(");
+            return false;
+        }
+        System.out.println("Now playing " + itr.next());
+        return true;
+    }
+
+    public static boolean playPrev(LinkedList <Song> playList){
+
+        if(!itr.hasNext()){
+            System.out.println("End of the PlayList.." + "\n" + "Nothing to play.. :(");
+            return false;
+        }
+        System.out.println("Now playing " + itr.next());
+        return true;
     }
 }
 
